00384386e2fa437ffea4f1583d9ff47c7b50dfb6
Nicolaspz <nicolaspz14@hotmail.com>
2022-10-19 15:38:47 +0100

    Inicinado o projecto

---
+++ src/test/java/com/nicolau/demoAjax/DemoAjaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolau.demoAjax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAjaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
